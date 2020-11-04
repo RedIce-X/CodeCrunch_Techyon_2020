ac8af07ea622daa69ed306d8fa186962dd41ca33
RedIce-X <raunaknaik99@gmail.com>
2020-11-04 14:22:51 +0530

    Added res.send for '/'

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.use(countryRoutes);
 app.use(covidRoutes);
 app.use(twitterRoutes);
 
+app.get('', (req, res) => {
+  res.send('running')
+})
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, (req, res) => {
